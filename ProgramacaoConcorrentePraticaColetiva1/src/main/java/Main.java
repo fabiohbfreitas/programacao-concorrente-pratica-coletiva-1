c459a799ff0b247c0787953f7000c4c856c8bec4
Daniel Ishida <ishidadecol@gmail.com>
2024-05-06 21:41:44 -0300

    Get random family size function
    Returns a random family size to create the family in a future implementation

--- ProgramacaoConcorrentePraticaColetiva1/src/main/java/Main.java
+++ ProgramacaoConcorrentePraticaColetiva1/src/main/java/Main.java
@@ -1,40 +1,57 @@
+import java.util.Random;
+
+
 public class Main {
-    public static void main(String[] args) {
-        var hotel = new Hotel();
-        for (var i = 0; i < 15; i++) {
-            var newGuest = new Guest("Guest " + i, hotel);
-            newGuest.start();
-        }
+	private static final int MIN_FAMILY_SIZE = 1;
+	private static final int MAX_FAMILY_SIZE = 6; // Change this to whatever maximum size you want
 
-        try {
-            Thread.sleep(35000);
-            System.out.println("================================");
-            System.out.println("Waiting: ");
-            for (var a : hotel.waitQueue) {
-                System.out.println("  "+a.getName());
-            }
+	public static void main(String[] args) {
+		var hotel = new Hotel();
 
-            System.out.println("In Rooms:");
-            for (var a: hotel.occupiedRooms) {
-                System.out.println("  " + a.guests.get(0).getName());
-            }
 
-            System.out.println("Available Rooms:");
-            for (var a: hotel.availableRooms) {
-                System.out.println("  " + a.name);
-            }
+		for (var i = 0; i < 15; i++) {
+			var newGuest = new Guest("Guest " + i, hotel);
+			newGuest.start();
+		}
 
-            System.out.println("Dirty Rooms:");
-            for (var a: hotel.awaitingCleaning) {
-                System.out.println("  " + a.name);
-            }
+		try {
+			Thread.sleep(35000);
+			System.out.println("================================");
+			System.out.println("Waiting: ");
+			for (var a : hotel.waitQueue) {
+				System.out.println("  "+a.getName());
+			}
 
-            System.out.println("Complaints: " + hotel.complaints);
+			System.out.println("In Rooms:");
+			for (var a: hotel.occupiedRooms) {
+				System.out.println("  " + a.guests.get(0).getName());
+			}
 
-            System.out.println("finishing...");
-            hotel.finished.set(true);
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-        }
-    }
+			System.out.println("Available Rooms:");
+			for (var a: hotel.availableRooms) {
+				System.out.println("  " + a.name);
+			}
+
+			System.out.println("Dirty Rooms:");
+			for (var a: hotel.awaitingCleaning) {
+				System.out.println("  " + a.name);
+			}
+
+			System.out.println("Complaints: " + hotel.complaints);
+
+			System.out.println("finishing...");
+			hotel.finished.set(true);
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+	}
+	
+	//MARK: Returns a random family size
+	public int randomFamilySize(){
+		Random random = new Random();
+		int randomNum;
+		randomNum = random.nextInt(MAX_FAMILY_SIZE + 1);
+
+		return randomNum;
+	}
 }
