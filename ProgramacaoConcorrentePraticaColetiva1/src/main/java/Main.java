408a2fd619cf496ef0ceb1fc64117d86e5af9aba
Daniel Ishida <ishidadecol@gmail.com>
2024-05-06 22:24:40 -0300

    Create family method
    Now creates a family instead of a single guest.

--- ProgramacaoConcorrentePraticaColetiva1/src/main/java/Main.java
+++ ProgramacaoConcorrentePraticaColetiva1/src/main/java/Main.java
@@ -2,16 +2,16 @@ import java.util.Random;
 
 
 public class Main {
-	private static final int MIN_FAMILY_SIZE = 1;
+	private static final int MIN_FAMILY_SIZE = 2;
 	private static final int MAX_FAMILY_SIZE = 6; // Change this to whatever maximum size you want
 
 	public static void main(String[] args) {
 		var hotel = new Hotel();
 
 
-		for (var i = 0; i < 15; i++) {
-			var newGuest = new Guest("Guest " + i, hotel);
-			newGuest.start();
+		for (var i = 0; i < 10; i++) {
+			var newfamily = createFamily(i, hotel);
+			
 		}
 
 		try {
@@ -47,11 +47,24 @@ public class Main {
 	}
 	
 	//MARK: Returns a random family size
-	public int randomFamilySize(){
+	public static int randomFamilySize(){
 		Random random = new Random();
 		int randomNum;
-		randomNum = random.nextInt(MAX_FAMILY_SIZE + 1);
+		randomNum = random.nextInt(MAX_FAMILY_SIZE + 1) + MIN_FAMILY_SIZE; //  Random integer in range of MIN_FAMILY_SIZE -> MAX_FAMILY_SIZE
 
 		return randomNum;
+	}
+	
+	//MARK: Returns an array of guests (family)
+	public static Guest[] createFamily(int familyNum, Hotel hotel) {
+		int familySize = randomFamilySize();
+		Guest[] newFamily = new Guest[familySize];
+		
+		for (var i = 0; i < familySize; i++) {
+			var newGuest = new Guest("Guest " + i + "from family" + familyNum, hotel); // Creates a new guest
+			newFamily[i] = newGuest; // appends new guest to it's family
+		}
+		
+		return newFamily;
 	}
 }
